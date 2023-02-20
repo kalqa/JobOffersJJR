c91047d4c1e8139981808b65e0bca55c6733d662
bartlomiejkalka <bartlomiejkalqa@gmail.com>
2023-02-20 13:48:17 +0100

    added docker-compose, added dependecies for itnegration tests, and spring boot added

---
+++ src/integration/java/com/junioroffers/features/TypicalScenarioUserWantToSeeOffersIntegrationTest.java
@@ -0,0 +1,26 @@
+package com.junioroffers.features;
+
+import com.junioroffers.BaseIntegrationTest;
+import org.junit.Test;
+
+public class TypicalScenarioUserWantToSeeOffersIntegrationTest extends BaseIntegrationTest {
+
+    @Test
+    public void user_want_to_see_offers_but_have_to_be_logged_in_and_external_server_should_have_some_offers(){
+//    step 1: there are no offers in external HTTP server
+//    step 2: scheduler ran 1st time and made GET to external server and system added 0 offers to database
+//    step 3: user tried to get JWT token by requesting POST /token with username=someUser, password=somePassword and system returned UNAUTHORIZED(401)
+//    step 4: user made GET /offers with no jwt token and system returned UNAUTHORIZED(401)
+//    step 5: user made POST /register with username=someUser, password=somePassword and system registered user with status OK(200)
+//    step 6: user tried to get JWT token by requesting POST /token with username=someUser, password=somePassword and system returned OK(200) and jwttoken=AAAA.BBBB.CCC
+//    step 7: user made GET /offers with header “Authorization: Bearer AAAA.BBBB.CCC” and system returned OK(200) with 0 offers
+//    step 8: there are 2 new offers in external HTTP server
+//    step 9: scheduler ran 2nd time and made GET to external server and system added 2 new offers with ids: 1000 and 2000 to database
+//    step 10: user made GET /offers with header “Authorization: Bearer AAAA.BBBB.CCC” and system returned OK(200) with 2 offers with ids: 1000 and 2000
+//    step 11: user made GET /offers/9999 and system returned NOT_FOUND(404) with message “Offer with id 9999 not found”
+//    step 12: user made GET /offers/1000 and system returned OK(200) with offer
+//    step 13: there are 2 new offers in external HTTP server
+//    step 14: scheduler ran 3rd time and made GET to external server and system added 2 new offers with ids: 3000 and 4000 to database
+//    step 15: user made GET /offers with header “Authorization: Bearer AAAA.BBBB.CCC” and system returned OK(200) with 4 offers with ids: 1000,2000, 3000 and 4000
+    }
+}
