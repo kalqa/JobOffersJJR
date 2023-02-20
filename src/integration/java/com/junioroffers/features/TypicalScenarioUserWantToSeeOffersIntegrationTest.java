24126149648a3dcca9c64d4b5a6a5bec5066854e
bartlomiejkalka <bartlomiejkalqa@gmail.com>
2023-02-20 21:00:01 +0100

    cleaned up integration test

--- src/integration/java/com/junioroffers/features/TypicalScenarioUserWantToSeeOffersIntegrationTest.java
+++ src/integration/java/com/junioroffers/features/TypicalScenarioUserWantToSeeOffersIntegrationTest.java
@@ -3,31 +3,20 @@ package com.junioroffers.features;
 import com.github.tomakehurst.wiremock.client.WireMock;
 import com.junioroffers.BaseIntegrationTest;
 import com.junioroffers.SampleJobOfferResponse;
-import com.junioroffers.domain.offer.OfferFetchable;
-import com.junioroffers.domain.offer.dto.JobOfferResponse;
-import java.util.List;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 
 
 public class TypicalScenarioUserWantToSeeOffersIntegrationTest extends BaseIntegrationTest implements SampleJobOfferResponse {
 
-    @Autowired
-    OfferFetchable offerHttpClient;
-
     @Test
-    public void user_want_to_see_offers_but_have_to_be_logged_in_and_external_server_should_have_some_offers() throws Exception {
+    public void user_want_to_see_offers_but_have_to_be_logged_in_and_external_server_should_have_some_offers() {
         //step 1: there are no offers in external HTTP server
-
-//        wireMockServer.stubFor(WireMock.get("/offers")
-//                .willReturn(WireMock.aResponse()
-//                        .withStatus(HttpStatus.OK.value())
-//                        .withHeader("Content-Type", "application/json")
-//                        .withBody(bodyWithZeroOffersJson())));
-
-
-        List<JobOfferResponse> jobOfferResponses = offerHttpClient.fetchOffers();
+        wireMockServer.stubFor(WireMock.get("/offers")
+                .willReturn(WireMock.aResponse()
+                        .withStatus(HttpStatus.OK.value())
+                        .withHeader("Content-Type", "application/json")
+                        .withBody(bodyWithZeroOffersJson())));
 
         //step 2: scheduler ran 1st time and made GET to external server and system added 0 offers to database
         //step 3: user tried to get JWT token by requesting POST /token with username=someUser, password=somePassword and system returned UNAUTHORIZED(401)
