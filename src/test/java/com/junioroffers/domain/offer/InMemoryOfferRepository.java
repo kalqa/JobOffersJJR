e49e4a2d578710ca2248644093293f73c9e62366
bartlomiejkalka <bartlomiejkalqa@gmail.com>
2023-02-21 14:42:25 +0100

    added repository mongo, and tested integration

--- src/test/java/com/junioroffers/domain/offer/InMemoryOfferRepository.java
+++ src/test/java/com/junioroffers/domain/offer/InMemoryOfferRepository.java
@@ -5,6 +5,13 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import java.util.stream.StreamSupport;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.query.FluentQuery;
 
 public class InMemoryOfferRepository implements OfferRepository {
 
@@ -20,24 +27,17 @@ public class InMemoryOfferRepository implements OfferRepository {
     }
 
     @Override
-    public Optional<Offer> findByOfferUrl(String offerUrl) {
-        return Optional.of(database.get(offerUrl));
-    }
-
-    @Override
-    public List<Offer> saveAll(List<Offer> offers) {
-        return offers.stream()
-                .map(this::save)
-                .toList();
-    }
-
-    @Override
     public Optional<Offer> findById(String id) {
         return Optional.ofNullable(database.get(id));
     }
 
     @Override
-    public Offer save(Offer entity) {
+    public boolean existsById(String s) {
+        return false;
+    }
+
+    @Override
+    public <S extends Offer> S save(S entity) {
         if (database.values().stream().anyMatch(offer -> offer.offerUrl().equals(entity.offerUrl()))) {
             throw new OfferDuplicateException(entity.offerUrl());
         }
@@ -50,11 +50,109 @@ public class InMemoryOfferRepository implements OfferRepository {
                 entity.offerUrl()
         );
         database.put(id.toString(), offer);
-        return offer;
+        return (S) offer;
+    }
+
+
+    @Override
+    public <S extends Offer> List<S> saveAll(Iterable<S> entities) {
+        return StreamSupport.stream(entities.spliterator(), false)
+                .map(this::save)
+                .toList();
     }
 
     @Override
     public List<Offer> findAll() {
         return database.values().stream().toList();
+    }
+
+    @Override
+    public Iterable<Offer> findAllById(Iterable<String> strings) {
+        return null;
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
+
+    @Override
+    public void deleteById(String s) {
+
+    }
+
+    @Override
+    public void delete(Offer entity) {
+
+    }
+
+    @Override
+    public void deleteAllById(Iterable<? extends String> strings) {
+
+    }
+
+    @Override
+    public void deleteAll(Iterable<? extends Offer> entities) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+
+    @Override
+    public List<Offer> findAll(Sort sort) {
+        return null;
+    }
+
+    @Override
+    public Page<Offer> findAll(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public <S extends Offer> S insert(S entity) {
+        return null;
+    }
+
+    @Override
+    public <S extends Offer> List<S> insert(Iterable<S> entities) {
+        return null;
+    }
+
+    @Override
+    public <S extends Offer> Optional<S> findOne(Example<S> example) {
+        return Optional.empty();
+    }
+
+    @Override
+    public <S extends Offer> List<S> findAll(Example<S> example) {
+        return null;
+    }
+
+    @Override
+    public <S extends Offer> List<S> findAll(Example<S> example, Sort sort) {
+        return null;
+    }
+
+    @Override
+    public <S extends Offer> Page<S> findAll(Example<S> example, Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public <S extends Offer> long count(Example<S> example) {
+        return 0;
+    }
+
+    @Override
+    public <S extends Offer> boolean exists(Example<S> example) {
+        return false;
+    }
+
+    @Override
+    public <S extends Offer, R> R findBy(Example<S> example, Function<FluentQuery.FetchableFluentQuery<S>, R> queryFunction) {
+        return null;
     }
 }
